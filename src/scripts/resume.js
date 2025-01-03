f4b170438c78c2f3f4095b8310ad968d9d47ad70
Mickey <badasstronaut.solutions@gmail.com>
2025-01-03 14:00:47 -0700

    Mobile view doesnt like my height hack -- just let it bounce around

--- src/scripts/resume.js
+++ src/scripts/resume.js
@@ -2,23 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const carousel = document.querySelector('.carousel');
     const slides = carousel.querySelectorAll('.carousel-slide');
 
-    function maxLengthSlideIdx() {
-        const maxLen = Math.max(...Array.from(slides).map(slide => slide.innerText.length));
-        return Array.from(slides).findIndex(slide => slide.innerText === maxLen);
-    }
-
     function showSlide(index) {
         slides.forEach(slide => slide.classList.remove('active'));
         currentSlide = (index + slides.length) % slides.length;
         slides[currentSlide].classList.add('active');
     }
 
-    // Load the longest rec first & set the height so we don't bounce
-    let currentSlide = maxLengthSlideIdx();
-    showSlide(currentSlide);
-    carousel.style.height =(slides[currentSlide].clientHeight + 15) + 'px';
-
-
+    currentSlide = 0;
     // Auto advance every 5 seconds
     let timer = setInterval(() => {
         showSlide(currentSlide + 1);
