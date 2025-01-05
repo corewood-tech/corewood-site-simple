be69362151be3d2c2535bd6ee136faaa6b8ee02b
Mickey <badasstronaut.solutions@gmail.com>
2025-01-04 18:36:23 -0700

    Added skills highlighting on position mouseover for resume page

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This has been iterated on significantly, to the point that shared templating was
 To keep things as simple as possible, and to support multiple domains and faciliate authoring more content, we added a simple `main.go` for packaging & dev-previewing the site.
 
 All utility functions are captured in the [`Makefile`](./Makefile).
+
+## Resume Page
+
+This page uses hard-coded logic in the html to add some pizazz. The `data-skill-ids` list corresponds to a `data-skill-id` in the skills lis. This is manipulated with the JavaScript for the resume page.
+
+Likewise, the references carousel js is in `assets/scripts/resume.js`.
--- dist/io/scripts/resume.js
+++ dist/io/scripts/resume.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+const configureCarousel = () => {
     const carousel = document.querySelector('.carousel');
     const slides = carousel.querySelectorAll('.carousel-slide');
 
@@ -31,4 +31,28 @@ document.addEventListener('DOMContentLoaded', function () {
         showSlide(currentSlide + 1);
         resetTimer();
     });
+}
+
+const highlightSkills = () => {
+    document.querySelectorAll('.experience-item').forEach(item => {
+        item.addEventListener('mouseover', () => {
+            skillIds = item.dataset.skillIds || [];
+            skillTags = Array.from(document.querySelectorAll('.skill-tag'));
+            
+            hlSkills = skillTags.filter(skill => skillIds.includes(skill.dataset.skillId));
+            skillTags.forEach(tag => tag.classList.remove('skill-highlight'));
+            hlSkills.forEach(tag => tag.classList.add('skill-highlight'));
+        });
+    });
+    document.querySelectorAll('.resume-section').forEach(section => {
+        section.addEventListener('mouseleave', () => {
+            skillTags = Array.from(document.querySelectorAll('.skill-tag'));
+            skillTags.forEach(tag => tag.classList.remove('skill-highlight'));
+        });
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    configureCarousel();
+    highlightSkills();
 });
